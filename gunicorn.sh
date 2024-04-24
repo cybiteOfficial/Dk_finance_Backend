8ecc2f16e62243347022f4b9af880a8af190030c
Tushar <yadavt147@gmail.com>
2024-04-24 17:34:03 +0000

    updated gunicorn env and js file

--- gunicorn.sh
+++ gunicorn.sh
@@ -4,4 +4,4 @@
    # Navigate to your Django project directory
    cd /home/ubuntu/Dk_finance_Backend
    # Start Gunicorn with your Django project
-   gunicorn Dev_Kripa_Finance.wsgi:application --bind 0.0.0.0:8000 --timeout 300
+   gunicorn Dev_Kripa_Finance.wsgi:application --bind 0.0.0.0:5000 --timeout 300
