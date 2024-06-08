f1d42d389e46284342b49cfe52c09bae8af336f4
ParaagSharma <pspsgxd@gmail.com>
2024-06-09 02:59:09 +0530

    Updated db

--- Dev_Kripa_Finance/settings/local.py
+++ Dev_Kripa_Finance/settings/local.py
@@ -7,10 +7,10 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME','dk_finance_local'),
-        'USER': os.environ.get('DB_USER','postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD','postgres'),
-        'HOST':os.environ.get('DB_HOST','localhost'),
+        'NAME': os.environ.get('DB_NAME','stage_inital'),
+        'USER': os.environ.get('DB_USER','adminStage'),
+        'PASSWORD': os.environ.get('DB_PASSWORD','dkFinanceStage'),
+        'HOST':os.environ.get('DB_HOST','db-staging.c1moyiyguvhg.ap-south-1.rds.amazonaws.com'),
         'PORT': os.environ.get('DB_PORT','5432')
     }
 }
