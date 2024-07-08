af63485c9d9694944352ba10d39a33a89321eac8
ParaagSharma <pspsgxd@gmail.com>
2024-07-09 02:31:29 +0530

    prod settings

--- Dev_Kripa_Finance/settings/prod.py
+++ Dev_Kripa_Finance/settings/prod.py
@@ -4,23 +4,23 @@ from .base import *
 DEBUG = os.environ.get('DEBUG')
 ALLOWED_HOSTS = ['3.111.52.245', '*']
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': os.environ.get('DB_NAME','stage_inital'),
-    #     'USER': os.environ.get('DB_USER','adminStage'),
-    #     'PASSWORD': os.environ.get('DB_PASSWORD','dkFinanceStage'),
-    #     'HOST':os.environ.get('DB_HOST','db-staging.c1moyiyguvhg.ap-south-1.rds.amazonaws.com'),
-    #     'PORT': os.environ.get('DB_PORT','5432')
-    # }
-
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME','cybite'),
-        'USER': os.environ.get('DB_USER','postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD','test'),
-        'HOST':os.environ.get('DB_HOST','localhost'),
+        'NAME': os.environ.get('DB_NAME','stage_inital'),
+        'USER': os.environ.get('DB_USER','adminStage'),
+        'PASSWORD': os.environ.get('DB_PASSWORD','dkFinanceStage'),
+        'HOST':os.environ.get('DB_HOST','db-staging.c1moyiyguvhg.ap-south-1.rds.amazonaws.com'),
         'PORT': os.environ.get('DB_PORT','5432')
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.environ.get('DB_NAME','cybite'),
+    #     'USER': os.environ.get('DB_USER','postgres'),
+    #     'PASSWORD': os.environ.get('DB_PASSWORD','test'),
+    #     'HOST':os.environ.get('DB_HOST','localhost'),
+    #     'PORT': os.environ.get('DB_PORT','5432')
+    # }
 
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
